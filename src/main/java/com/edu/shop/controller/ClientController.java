4f7b76fb6920a5f7fc176a353f4a5ee739a94d6b
Cesar Choccata <c21choccata@gmail.com>
2025-01-30 23:25:36 -0500

    Add ClientController

---
+++ src/main/java/com/edu/shop/controller/ClientController.java
@@ -0,0 +1,36 @@
+package com.edu.shop.controller;
+
+import com.edu.shop.dto.ClientDTO;
+import com.edu.shop.serviceImpls.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("client")
+public class ClientController {
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping("add")
+    public ResponseEntity<ClientDTO> save(@RequestBody ClientDTO clientDTO){
+        ClientDTO clientDto2 = clientService.save(clientDTO);
+        return new ResponseEntity<>(clientDto2, HttpStatus.CREATED);
+
+    }
+    @GetMapping("all")
+    public ResponseEntity<List<ClientDTO>> all(){
+        return new ResponseEntity<>(clientService.getAllClients(),HttpStatus.OK);
+    }
+    @PatchMapping(path = "update")
+    public ResponseEntity<ClientDTO> update(@RequestBody ClientDTO clientDTO){
+        return new ResponseEntity<>(this.clientService.update(clientDTO).get(),HttpStatus.OK);
+    }
+
+
+
+
+}
