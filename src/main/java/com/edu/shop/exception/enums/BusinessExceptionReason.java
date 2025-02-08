1d650a41b2626d4c5bcc45aef0c57cb70167c645
Cesar Choccata <c21choccata@gmail.com>
2025-02-08 18:34:23 -0500

    Fix HTTP status codes for business exceptions

--- src/main/java/com/edu/shop/exception/enums/BusinessExceptionReason.java
+++ src/main/java/com/edu/shop/exception/enums/BusinessExceptionReason.java
@@ -6,27 +6,20 @@ import org.springframework.http.HttpStatus;
 public enum BusinessExceptionReason implements BusinessExceptionPolicy {
 
 
-    CLIENT_WITH_EMAIL_EXITS("El cliente con el email %s ya existe", HttpStatus.NOT_FOUND),
+    CLIENT_WITH_EMAIL_EXITS("El cliente con el email %s ya existe", HttpStatus.CONFLICT),
     ENTITY_NOT_FOUND("%s, no existe",HttpStatus.NOT_FOUND),
     PRODUCT_STOCK_UNAVAILABLE("No hay suficiente stock disponible para el producto %s",HttpStatus.CONFLICT),
     ENTITY_CONFICT("%s",HttpStatus.CONFLICT),
-    ENTITY_EXITS("%s ya existe ",HttpStatus.NOT_FOUND);
+    ENTITY_EXITS("%s ya existe ",HttpStatus.CONFLICT);
 
     private final String code;
     private final String message;
     private final HttpStatus httpStatus;
 
-    //se inicializa automaticament
     BusinessExceptionReason(String message, HttpStatus httpStatus) {
-        this.code = BusinessExceptionReason.class.getSimpleName();// el cod retorar el nombre de la clase en tiempo de ejecucion
+        this.code = BusinessExceptionReason.class.getSimpleName();
         this.message = message;
         this.httpStatus = httpStatus;
-
-        /*
-         * enum constructors are used to initialize the constants with specific values.
-         * These constructors are implicitly private and cannot be invoked directly;
-         * they are called automatically when each enum constant is created.
-         * */
     }
 
     @Override
